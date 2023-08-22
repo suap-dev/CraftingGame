bf89b05581178708746dde9fe09cc1da316ab5d2
suap-dev <suap.dev@gmail.com>
2023-08-22 21:37:12 +0200

    Add project files.

---
+++ Source/CraftingGame/CraftingGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CraftingGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRAFTINGGAME_API ACraftingGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
